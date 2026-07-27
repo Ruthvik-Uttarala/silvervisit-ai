416846d634edb37b2b4c78c71e7884921116d74d
Ruthvik Uttarala <123519578+Ruthvik-Uttarala@users.noreply.github.com>
2026-07-27 13:19:32 -0400

    Remove temporary full-workflow diagnostic endpoint

--- backend/api/full-workflow.ts
+++
@@ -1,7 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
-import { randomUUID } from "node:crypto";
-import { handleFullWorkflowDiagnostic } from "../src/routes/fullWorkflowDiagnostic";
-
-export default async function handler(_req: IncomingMessage, res: ServerResponse): Promise<void> {
-  await handleFullWorkflowDiagnostic(res, randomUUID());
-}
